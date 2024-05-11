388e369379e3386f267619e21faa00568af02591
BryanWHansen <bhansen1717@outlook.com>
2024-05-11 13:45:19 -0400

    committing simple react app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+import Button from "./components/button";
+
+function App() {
+  let cities = ['Houston', 'Chicago', 'New York', 'Atlanta'];
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+  const [alertVisible, setAlertVisible] = useState(false);
+  return (
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+      <Button color='primary' onclick={() => setAlertVisible(true)}>My Button</Button>
+      <ListGroup items={cities} heading="cities" onSelectItem={handleSelectItem}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface ButtonProps {
+    children: string;
+    color?: 'primary' | 'secondary' | 'warning';
+    onclick: () => void
+}
+
+const Button = ({ children, color = 'primary', onclick }: ButtonProps) => {
+  return (
+    <button className={'btn btn-' + color} onClick={onclick}>{children}</button>
+  )
+}
+
+export default Button
